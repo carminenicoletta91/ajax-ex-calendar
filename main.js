012813dc83f017f7746cb1aa9d911fd987f32bea
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-07-31 22:44:24 +0200

    --creata fuznione nel success per poter inserire le festività ai relativi giorni--

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ $(document).ready(function() {
     }
     // dichiaro variabile per ritornarmi il valore del mese tramite attr rif
     var mese=$(".box-carousel >div#mesi >div.active").attr("rif");
-    console.log(mese);
+    // console.log(mese);
     // dichiaro variabile che crea la data del mese corrispondente
     var data = moment("2018-"+mese+"-01","YYYY-MM-DD");
-    console.log(data);
+    // console.log(data);
     for(var i=1;i<=data.daysInMonth();i++){
     $(".box-carousel >div#mesi >div.active ul").append ("<li>"+data.format(i+"-MMMM-YYYY")+ "</li>");
   }
@@ -30,8 +30,24 @@ $(document).ready(function() {
       url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month="+number,
       method:"GET",
       success:function(data){
-        console.log(data.response);
-      }
+
+        $(".box-carousel >div#mesi >div.active ul li").each(
+          function(){
+            for(var i=0;i<data.response.length;i++){
+              var j=i;
+              var datamese = data.response[i].date;
+              var nuovadata =moment(datamese).format("D-MMMM-YYYY");
+              // console.log(nuovadata);
+
+            var miastringa = $(this).text();
+            // console.log("LA TUA STRINGA:"+miastringa);
+            if(nuovadata === miastringa ){
+              var metti = $(this).text(miastringa +"-"+ data.response[j].name)
+              console.log("hai vinto "+metti);
+            }
+            }
+               })
+      },
     })
   });
   // appena aggiorno pagina
@@ -47,8 +63,31 @@ $(document).ready(function() {
     url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month="+number,
     method:"GET",
     success:function(data){
-      console.log(data.response);
-    }
+       console.log(data.response);
+      // // for(var i=0;i<data.response.length;i++){
+      // //   console.log(data.response[i]);
+      // //   var datamese = data.response[i].date;
+      // //   var nuovadata =moment(datamese).format("D-MMMM-YYYY");
+      //   console.log(nuovadata);
+
+      $(".box-carousel >div#mesi >div.active ul li").each(
+        function(){
+          for(var i=0;i<data.response.length;i++){
+            var j=i;
+            var datamese = data.response[i].date;
+            var nuovadata =moment(datamese).format("D-MMMM-YYYY");
+            console.log(nuovadata);
+          var miastringa = $(this).text();
+          console.log("LA TUA STRINGA:"+miastringa);
+          if(nuovadata === miastringa ){
+            var metti = $(this).html(miastringa +"-"+ data.response[j].name)
+            console.log(metti);
+          }
+          }
+         })
+       }
+
+
   });
   // funzione per visualizzare il mese successivo
   function nextdiv(){
